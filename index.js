f8636a83c0f9afa2c33425a35d68cfc0e17ecb8a
Novan_ <83200466+nov4n-4r@users.noreply.github.com>
2022-11-07 03:53:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,23 @@
 const express = require("express")
-const cors = require("cors")
+
 const app = express()
 
-app.set("view engine", "ejs")
-app.use(cors())
+const isNan = target => !(parseInt(target) === parseInt(target))
+const isNumber = target => (/^[0-9]+$/).test(target)
 
-app.get(`/`, (req,res) => {
-    res.send(`
-    <div>
-        <p>Example : <a href="/api/2022-07-12"> /api/2022-07-12 </a></p>
-        <p>Example : <a href="/api/1657584000000"> /api/1657584000000 </a></p>
-        <p> It'll be the same </p>
-    </div>
-    `)
+app.get("/api/:date", (req, res) => {
+  console.log(req.params)
+  const arg = !isNumber(req.params.date) ? 
+      req.params.date : parseInt(req.params.date)
+
+  const date = new Date(arg)
+
+  if(date.toString() === "Invalid Date") return res.json({error : date.toString()})
+
+  return res.json({
+    unix : date.getTime(),
+    utc : date.toUTCString()
+  })
 })
 
-app.get("/api/", (req,res) => {
-    const date = new Date()
-    res.json({
-        unix : date.getTime(),
-        utc : date.toUTCString()
-    })
-})
-
-app.get(`/api/:date`, (req, res) => {
-    let query = req.params.date
-    const type1 = new Date(query) 
-    const type2 = new Date(parseInt(query))
-
-    if(type1.toString() !== "Invalid Date"){
-        console.log(type1);
-        const date = type1
-        return res.json({
-            unix : date.getTime(),
-            utc : date.toUTCString()
-        })
-    }else if(type2.toString() !== "Invalid Date"){
-        const pattern = /\D/g
-        if(pattern.test(query)) return res.json({error : "Invalid Date"})
-        const date = type2
-        return res.json({
-            unix : date.getTime(),
-            utc : date.toUTCString()
-        })
-    }else{
-        return res.json({
-            error : "Invalid Date"
-        })
-    }
-})
-
-app.listen(4000)
+app.listen(80, () => console.log("Server running at port 80..."))
